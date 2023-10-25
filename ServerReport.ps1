737c3f75a1d62e671b55e26d4a1ad9e72a1a4c0b
jacobmccutchen96 <mccutchenjacob@gmail.com>
2023-10-25 14:51:05 -0400

    Added disk info

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -1,6 +1,10 @@
 #Computer name and OS Version save and display to variable
-$info = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
-$info
+$Computerinfo = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
+$Computerinfo
+#Get Disk info. No specific info for number of drives, use global count method for listed items
+$DiskInfo = Get-Disk
+$DiskInfo.count
+
 
 
 #Save all info into the variable, then display variable  and sort to specific info
