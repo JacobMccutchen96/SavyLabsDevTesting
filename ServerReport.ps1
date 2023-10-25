eae2e651b68e924200d7747ffc8a709bba14a960
jacobmccutchen96 <mccutchenjacob@gmail.com>
2023-10-25 14:17:39 -0400

    Get CompName and OS Version

--- ServerReport.ps1
+++ ServerReport.ps1
@@ -1,4 +1,12 @@
-#Computer name save and display to variable
-$info = Get-ComputerInfo | Select-Object CsDNSHostName
+#Computer name and OS Version save and display to variable
+$info = Get-ComputerInfo | Select-Object CsDNSHostName, OsVersion
 $info
+
+
+#Save all info into the variable, then display variable  and sort to specific info
+#Will display as string value instead of object values
+#$info = Get-ComputerInfo
+#$info.CsDNSHostName
+#$info.OsVersion
+
 
